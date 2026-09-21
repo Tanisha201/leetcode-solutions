38695f237d006edb17c2d15d0795d6572b8b7209
Tanisha201 <tanishakathpalia@gmail.com>
2026-09-22 01:31:30 +0530

    Time: 73 ms (50.04%) | Memory: 111.2 MB (73.99%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>seen;
+        for (int i = 0; i < nums.size(); i++) {
+            if (seen[nums[i]]==1){
+                return true;
+            }
+            seen[nums[i]]=1;
+        }
+        return false;
+    }
+};
