e9118e8fd2f47a380193a108ad90970a8d015d4f
Tanisha201 <tanishakathpalia@gmail.com>
2026-09-23 00:02:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.14%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()){
+            return false;
+        }
+        unordered_map<char,int> seen;
+
+        for (char c : s) {
+            seen[c]++;
+        }
+        for (char c : t) {
+            seen[c]--;
+        }
+        for (auto x : seen){
+            if (x.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
